a6b7ee59e3702c4897c05b2225733863d426d983
yegetables <yegetables@gmail.com>
2024-08-19 04:01:51 +0800

    first customs

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,19 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+#echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
+
+# small-package 有bug
+#echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
+
+#echo '#src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+#echo '#src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+#rm -rf feeds/luci/applications/luci-app-mosdns
+#rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+#rm -rf feeds/packages/utils/v2dat
+# rm -rf feeds/packages/lang/golang
+# git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+

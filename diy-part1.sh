bb2cfe81a4bb2115935343569dc43de5127bac88
yegetables <yegetables@gmail.com>
2024-08-19 20:29:47 +0800

    3

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,8 +22,8 @@
 # small-package 有bug
 #echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
 
-echo "src-git openwrtpackage https://github.com/kenzok8/openwrt-packages ">>feeds.conf.default
-echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
+echo "src-git openwrtpackage https://github.com/kenzok8/openwrt-packages " >>feeds.conf.default
+echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> feeds.conf.default
 
 #echo '#src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo '#src-git small https://github.com/kenzok8/small' >>feeds.conf.default
